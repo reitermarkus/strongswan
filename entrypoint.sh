d085028943e40bbd2808ef09505b1c45b493bd21
Markus Reiter <me@reitermark.us>
2021-05-15 00:04:02 +0200

    Make UUIDs deterministic.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,19 +4,24 @@ set -euo pipefail
 
 VPN_DOMAIN_REVERSED="$(tr '.' $'\n' <<< "${VPN_DOMAIN}" | tac | paste -s -d '.')"
 
-mkdir -p /etc/ipsec.d/{cacerts,certs,private}
+IPSEC_DIR='/etc/ipsec.d'
+
+mkdir -p "${IPSEC_DIR}"/{cacerts,certs,private}
 
 CA_NAME="${VPN_NAME} Root CA"
-CA_KEY=/etc/ipsec.d/private/ca.pem
-CA_CERT=/etc/ipsec.d/cacerts/ca.cert.pem
+CA_KEY="${IPSEC_DIR}/private/ca.pem"
+CA_CERT_BASENAME='ca.cert.pem'
+CA_CERT="${IPSEC_DIR}/cacerts/${CA_CERT_BASENAME}"
 
-SERVER_KEY=/etc/ipsec.d/private/server.pem
-SERVER_CERT=/etc/ipsec.d/certs/server.cert.pem
+SERVER_KEY="${IPSEC_DIR}/private/server.pem"
+SERVER_CERT_BASENAME='server.cert.pem'
+SERVER_CERT="${IPSEC_DIR}/certs/${SERVER_CERT_BASENAME}"
 
-CLIENT_KEY=/etc/ipsec.d/private/client.pem
-CLIENT_CERT=/etc/ipsec.d/certs/client.cert.pem
-CLIENT_CERT_P12=/etc/ipsec.d/client.cert.p12
-CLIENT_MOBILECONFIG=/etc/ipsec.d/client.mobileconfig
+CLIENT_KEY="${IPSEC_DIR}/private/client.pem"
+CLIENT_CERT="${IPSEC_DIR}/certs/client.cert.pem"
+CLIENT_CERT_P12_BASENAME='client.cert.p12'
+CLIENT_CERT_P12="${IPSEC_DIR}/${CLIENT_CERT_P12_BASENAME}"
+CLIENT_MOBILECONFIG="${IPSEC_DIR}/client.mobileconfig"
 
 if ! [[ -f "${CA_KEY}" ]]; then
   ipsec pki --gen --type rsa --size 4096 --outform pem > "${CA_KEY}"
@@ -102,11 +107,12 @@ cat > /etc/ipsec.secrets <<EOF
 : RSA "$(basename "${SERVER_KEY}")"
 EOF
 
-UUID1="$(uuidgen)"
-UUID2="$(uuidgen)"
-UUID3="$(uuidgen)"
-UUID4="$(uuidgen)"
-UUID5="$(uuidgen)"
+UUID_NAMESPACE="$(uuidgen --sha1 --namespace @dns --name "${VPN_DOMAIN}")"
+UUID_CA_CERT="$(uuidgen --sha1 --namespace "${UUID_NAMESPACE}" --name "${CA_CERT_BASENAME}")"
+UUID_SERVER_CERT="$(uuidgen --sha1 --namespace "${UUID_NAMESPACE}" --name "${SERVER_CERT_BASENAME}")"
+UUID_P12_CERT="$(uuidgen --sha1 --namespace "${UUID_NAMESPACE}" --name "${CLIENT_CERT_P12_BASENAME}")"
+UUID_VPN_SETTINGS="$(uuidgen --sha1 --namespace "${UUID_NAMESPACE}" --name 'com.apple.vpn.managed')"
+UUID_CONFIGURATION="$(uuidgen --sha1 --namespace "${UUID_NAMESPACE}" --name 'configuration')"
 
 cat > "${CLIENT_MOBILECONFIG}" <<EOF
 <?xml version="1.0" encoding="UTF-8"?>
@@ -116,44 +122,97 @@ cat > "${CLIENT_MOBILECONFIG}" <<EOF
   <key>PayloadContent</key>
   <array>
     <dict>
-      <key>Password</key>
-      <string>${VPN_P12_PASSWORD}</string>
-      <key>PayloadCertificateFileName</key>
-      <string>client.cert.p12</string>
-      <key>PayloadContent</key>
-      <data>$(base64 "${CLIENT_CERT_P12}")</data>
-      <key>PayloadDescription</key>
-      <string>PKCS12 Certificate</string>
-      <key>PayloadDisplayName</key>
-      <string>${VPN_NAME} Client Certificate</string>
-      <key>PayloadIdentifier</key>
-      <string>com.apple.security.pkcs12.${UUID1}</string>
-      <key>PayloadType</key>
-      <string>com.apple.security.pkcs12</string>
-      <key>PayloadUUID</key>
-      <string>${UUID1}</string>
-      <key>PayloadVersion</key>
-      <integer>1</integer>
-    </dict>
-    <dict>
-      <key>PayloadCertificateFileName</key>
-      <string>ca.cer</string>
-      <key>PayloadContent</key>
-      <data>$(base64 "${CA_CERT}")</data>
-      <key>PayloadDescription</key>
-      <string>CA Certificate</string>
-      <key>PayloadDisplayName</key>
-      <string>${CA_NAME}</string>
-      <key>PayloadIdentifier</key>
-      <string>com.apple.security.root.${UUID2}</string>
       <key>PayloadType</key>
       <string>com.apple.security.root</string>
       <key>PayloadUUID</key>
-      <string>${UUID2}</string>
+      <string>${UUID_CA_CERT}</string>
+      <key>PayloadIdentifier</key>
+      <string>com.apple.security.root.${UUID_CA_CERT}</string>
       <key>PayloadVersion</key>
       <integer>1</integer>
+      <key>PayloadDisplayName</key>
+      <string>${CA_NAME}</string>
+      <key>PayloadDescription</key>
+      <string>CA Certificate</string>
+      <key>PayloadCertificateFileName</key>
+      <string>${CA_CERT_BASENAME}</string>
+      <key>PayloadContent</key>
+      <data>$(base64 "${CA_CERT}")</data>
     </dict>
     <dict>
+      <key>PayloadType</key>
+      <string>com.apple.security.pkcs1</string>
+      <key>PayloadUUID</key>
+      <string>${UUID_SERVER_CERT}</string>
+      <key>PayloadIdentifier</key>
+      <string>com.apple.security.pkcs1.${UUID_SERVER_CERT}</string>
+      <key>PayloadVersion</key>
+      <integer>1</integer>
+      <key>PayloadDisplayName</key>
+      <string>${VPN_NAME} Server Certificate</string>
+      <key>PayloadDescription</key>
+      <string>PKCS1 Certificate</string>
+      <key>PayloadCertificateFileName</key>
+      <string>${SERVER_CERT_BASENAME}</string>
+      <key>PayloadContent</key>
+      <data>$(base64 "${SERVER_CERT}")</data>
+    </dict>
+    <dict>
+      <key>PayloadType</key>
+      <string>com.apple.security.pkcs12</string>
+      <key>PayloadUUID</key>
+      <string>${UUID_P12_CERT}</string>
+      <key>PayloadIdentifier</key>
+      <string>com.apple.security.pkcs12.${UUID_P12_CERT}</string>
+      <key>PayloadVersion</key>
+      <integer>1</integer>
+      <key>PayloadDisplayName</key>
+      <string>${VPN_NAME} Client Certificate</string>
+      <key>PayloadDescription</key>
+      <string>PKCS12 Certificate</string>
+      <key>PayloadCertificateFileName</key>
+      <string>${CLIENT_CERT_P12_BASENAME}</string>
+      <key>PayloadContent</key>
+      <data>$(base64 "${CLIENT_CERT_P12}")</data>
+      <key>Password</key>
+      <string>${VPN_P12_PASSWORD}</string>
+    </dict>
+    <dict>
+      <key>PayloadType</key>
+      <string>com.apple.vpn.managed</string>
+      <key>PayloadUUID</key>
+      <string>${UUID_VPN_SETTINGS}</string>
+      <key>PayloadIdentifier</key>
+      <string>com.apple.vpn.managed.${UUID_VPN_SETTINGS}</string>
+      <key>PayloadVersion</key>
+      <real>1</real>
+      <key>PayloadDisplayName</key>
+      <string>${VPN_NAME}</string>
+      <key>PayloadDescription</key>
+      <string>VPN Settings</string>
+      <key>Proxies</key>
+      <dict>
+        <key>HTTPEnable</key>
+        <integer>0</integer>
+        <key>HTTPSEnable</key>
+        <integer>0</integer>
+      </dict>
+      <key>IPv4</key>
+      <dict>
+        <key>OverridePrimary</key>
+        <integer>0</integer>
+      </dict>
+      <key>DNS</key>
+      <dict>
+        <key>SearchDomains</key>
+        <array>
+          <string>local</string>
+        </array>
+      </dict>
+      <key>UserDefinedName</key>
+      <string>${VPN_NAME} (IKEv2)</string>
+      <key>VPNType</key>
+      <string>IKEv2</string>
       <key>IKEv2</key>
       <dict>
         <key>AuthenticationMethod</key>
@@ -193,7 +252,7 @@ cat > "${CLIENT_MOBILECONFIG}" <<EOF
         <key>LocalIdentifier</key>
         <string>client@${VPN_DOMAIN}</string>
         <key>PayloadCertificateUUID</key>
-        <string>${UUID1}</string>
+        <string>${UUID_P12_CERT}</string>
         <key>RemoteAddress</key>
         <string>${VPN_DOMAIN}</string>
         <key>RemoteIdentifier</key>
@@ -232,73 +291,20 @@ cat > "${CLIENT_MOBILECONFIG}" <<EOF
           </dict>
         </array>
       </dict>
-      <key>IPv4</key>
-      <dict>
-        <key>OverridePrimary</key>
-        <integer>0</integer>
-      </dict>
-      <key>PayloadDescription</key>
-      <string>VPN Settings</string>
-      <key>PayloadDisplayName</key>
-      <string>${VPN_NAME}</string>
-      <key>PayloadIdentifier</key>
-      <string>com.apple.vpn.managed.${UUID3}</string>
-      <key>PayloadType</key>
-      <string>com.apple.vpn.managed</string>
-      <key>PayloadUUID</key>
-      <string>${UUID3}</string>
-      <key>PayloadVersion</key>
-      <real>1</real>
-      <key>Proxies</key>
-      <dict>
-        <key>HTTPEnable</key>
-        <integer>0</integer>
-        <key>HTTPSEnable</key>
-        <integer>0</integer>
-      </dict>
-      <key>DNS</key>
-      <dict>
-        <key>SearchDomains</key>
-        <array>
-          <string>local</string>
-        </array>
-      </dict>
-      <key>UserDefinedName</key>
-      <string>${VPN_NAME} (IKEv2)</string>
-      <key>VPNType</key>
-      <string>IKEv2</string>
-    </dict>
-    <dict>
-      <key>PayloadCertificateFileName</key>
-      <string>server.cer</string>
-      <key>PayloadContent</key>
-      <data>$(base64 "${SERVER_CERT}")</data>
-      <key>PayloadDescription</key>
-      <string>PKCS1 Certificate</string>
-      <key>PayloadDisplayName</key>
-      <string>${VPN_NAME} Server Certificate</string>
-      <key>PayloadIdentifier</key>
-      <string>com.apple.security.pkcs1.${UUID4}</string>
-      <key>PayloadType</key>
-      <string>com.apple.security.pkcs1</string>
-      <key>PayloadUUID</key>
-      <string>${UUID4}</string>
-      <key>PayloadVersion</key>
-      <integer>1</integer>
     </dict>
   </array>
-  <key>PayloadDisplayName</key>
-  <string>${VPN_NAME}</string>
-  <key>PayloadIdentifier</key>
-  <string>${VPN_DOMAIN_REVERSED}</string>
-  <key>PayloadRemovalDisallowed</key>
-  <false/>
   <key>PayloadType</key>
   <string>Configuration</string>
   <key>PayloadUUID</key>
-  <string>${UUID5}</string>
+  <string>${UUID_CONFIGURATION}</string>
+  <key>PayloadIdentifier</key>
+  <string>${VPN_DOMAIN_REVERSED}</string>
   <key>PayloadVersion</key>
   <integer>1</integer>
+  <key>PayloadDisplayName</key>
+  <string>${VPN_NAME}</string>
+  <key>PayloadRemovalDisallowed</key>
+  <false/>
 </dict>
 </plist>
 EOF
